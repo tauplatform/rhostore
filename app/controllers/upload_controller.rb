45db153a17d4d7893174850843b7164a94150d50
vmusulainen <vmusulainen@gmail.com>
2017-01-17 18:45:00 +0300

    Uploading added

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -3,7 +3,8 @@ require 'fileutils'
 class UploadController < ApplicationController
   def new
     respond_to do |format|
-      msg = {:status => "ok", :message => "Success!", :html => "<b>...</b>"}
+      puts "new params #{params}"
+      msg = {:status => "ok", :filename => @params[:filename]}
       format.json { render :json => msg } # don't do msg.to_json
     end
   end
@@ -20,6 +21,6 @@ class UploadController < ApplicationController
 
     path = File::join(Rails.public_path, 'images', 'upload', filename)
     File.open(path, 'wb') { |f| f.write(params[:file].read) }
-    redirect_to '/upload/new'
+    redirect_to '/upload/new', :filename => "https://taustore.herokuapp.com/images/upload/#{filename}"
   end
 end
