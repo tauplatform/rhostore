c684168782fe82e919a27b21e4e04204f544c85b
vmusulainen <vmusulainen@gmail.com>
2017-01-17 11:07:09 +0300

    Uploading added

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -5,11 +5,10 @@ class UploadController < ApplicationController
 
   def create
     puts "upload params: #{params}"
-    name = params[:upload][:file].original_filename
+    name = params[:file].original_filename
     directory = "public/images/upload"
     path = File.join(directory, name)
-    File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
+    File.open(path, "wb") { |f| f.write(params[:file].read) }
     flash[:notice] = "File uploaded"
-    redirect_to "/upload/new"
   end
 end
