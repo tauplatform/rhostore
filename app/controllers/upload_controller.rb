d345477def23ff6045ce38634f118dbf98f4db1a
vmusulainen <vmusulainen@gmail.com>
2017-01-17 18:38:15 +0300

    Uploading added

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -2,7 +2,10 @@ require 'fileutils'
 
 class UploadController < ApplicationController
   def new
-
+    respond_to do |format|
+      msg = {:status => "ok", :message => "Success!", :html => "<b>...</b>"}
+      format.json { render :json => msg } # don't do msg.to_json
+    end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rhostore::Application.routes.draw do
 
   resources :customers
 
-  resources :upload
+  resources :upload, defaults: {format: :json}
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
